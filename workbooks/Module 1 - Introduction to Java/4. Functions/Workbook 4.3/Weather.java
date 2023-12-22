7970fc61ab657e64551db8aa660d7c8aaec8af07
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2023-12-22 19:17:36 +0100

    save

--- workbooks/Module 1 - Introduction to Java/4. Functions/Workbook 4.3/Weather.java
+++ workbooks/Module 1 - Introduction to Java/4. Functions/Workbook 4.3/Weather.java
@@ -4,22 +4,26 @@ public class Weather {
         double evening = 61;       //temperature during the evening in fahrenheit
         double midnight = 55;      //temperature at midnight in fahrenheit
 
-        //Task 3 - Call the printTemperatures function. 
-        
-        
+        //Task 3 - Call the printTemperatures function.
+        printTemperatures(noon, fahrenheitToCelsius(noon));
+        printTemperatures(evening, fahrenheitToCelsius(evening));
+        printTemperatures(midnight, fahrenheitToCelsius(midnight));
     }
 
 
     //Task 1: Make a function here. See the doc comments below. 
 
     /**
-     * Function name: fahrenheitToCelsius - converts fahrenheit to celcius
-     * @param fahrenheit (double)
-     * @return celsius (double)
+     * 
+     * @param fahrenheit 
+     * @return 
      * 
      * Inside the function:
      * 1. returns the temperature in celcius. C = (F - 32) * 5/9. 
      */
+     public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+     }
 
      
     //Task 2: Make a function here. See the doc comments below. 
@@ -32,4 +36,8 @@ public class Weather {
      *  1. prints: F: <temperature in fahrenheit>.
      *  2. prints: C: <temperature in celsius> . 
      */
+    public static void printTemperatures(double fahrenheit, double celsius) {
+        System.out.println("\nFahrenheit: " + fahrenheit);
+        System.out.println("Celsius: " + celsius);
+    }
 }
