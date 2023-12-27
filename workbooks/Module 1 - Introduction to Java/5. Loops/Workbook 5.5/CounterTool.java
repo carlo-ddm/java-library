f6f366267db48506d90b13f414e02d141198a64f
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2023-12-27 16:44:50 +0100

    start

--- workbooks/Module 1 - Introduction to Java/5. Loops/Workbook 5.5/CounterTool.java
+++ workbooks/Module 1 - Introduction to Java/5. Loops/Workbook 5.5/CounterTool.java
@@ -8,6 +8,19 @@ public class CounterTool {
         System.out.println("Oh, ok...");
 
        // See detailed instructions on Learn the Part.
+       System.out.print(" Pick a number to count by: ");
+       int countBy = scan.nextInt();
+
+       System.out.print(" Pick to start counting from: ");
+       int countingFrom = scan.nextInt();
+
+       System.out.print(" Pick a number to count to: ");
+       int countTo = scan.nextInt();
+
+       for (int i = countingFrom; i <= countTo; i += countBy) {
+        System.out.println(i);
+       }
+
 
         scan.close();
     }
