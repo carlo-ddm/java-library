af33873147c088c00a76ca5732eabe1040f17ec2
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2023-12-27 16:18:30 +0100

    save

--- workbooks/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
+++ workbooks/Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
@@ -5,6 +5,13 @@ public class CountingTool {
         Scanner scan = new Scanner(System.in);
 
         // See Learn the Part for the instructions.
+        System.out.print("Hi Carlo! Choose a number to count to: ");
+        int limit = scan.nextInt();
+
+        System.out.println("Great! Here's how it's done");
+        for (int i = 0; i <= limit; i++) {
+            System.out.print(i + " ");
+        }
 
         scan.close();
     }
