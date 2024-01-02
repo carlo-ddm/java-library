64ba4a0e95f5e83197e787df5c4b8d3fefb505fb
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2024-01-02 16:47:41 +0100

    save

--- workbooks/Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
+++ workbooks/Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
@@ -203,6 +203,17 @@ public class TicTacToe {
 
   public static int checkRight(char[][] board) {
     int count = 0;
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board.length; j++) {
+        if (board[i][i] == 'X') {
+          count++;
+        }
+
+        if (board[i][i] == 'O') {
+          count--;
+        }
+      }
+    }
     return count;
   }
 
