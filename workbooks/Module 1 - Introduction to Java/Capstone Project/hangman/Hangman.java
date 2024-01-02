0304cf6be902d4a0aa3365a1cedf84b505416966
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2024-01-02 17:36:55 +0100

    save

--- workbooks/Module 1 - Introduction to Java/Capstone Project/hangman/Hangman.java
+++ workbooks/Module 1 - Introduction to Java/Capstone Project/hangman/Hangman.java
@@ -1,83 +1,95 @@
 public class Hangman {
 
-    public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
-    "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
-    "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
-    "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose",
-    "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", 
-    "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal",
-    "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan",
-    "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
-    "wombat", "zebra"};
+    public static String[] words = { "ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
+            "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
+            "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
+            "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose",
+            "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon",
+            "python", "rabbit", "ram", "rat", "raven", "rhino", "salmon", "seal",
+            "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan",
+            "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
+            "wombat", "zebra" };
 
-    public static String[] gallows = {"+---+\n" +
-    "|   |\n" +
-    "    |\n" +
-    "    |\n" +
-    "    |\n" +
-    "    |\n" +
-    "=========\n",
+    public static String[] gallows = { "+---+\n" +
+            "|   |\n" +
+            "    |\n" +
+            "    |\n" +
+            "    |\n" +
+            "    |\n" +
+            "=========\n",
 
-    "+---+\n" +
-    "|   |\n" +
-    "O   |\n" +
-    "    |\n" +
-    "    |\n" +
-    "    |\n" +
-    "=========\n",
+            "+---+\n" +
+                    "|   |\n" +
+                    "O   |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "=========\n",
 
-    "+---+\n" +
-    "|   |\n" +
-    "O   |\n" +
-    "|   |\n" +
-    "    |\n" +
-    "    |\n" +
-    "=========\n",
+            "+---+\n" +
+                    "|   |\n" +
+                    "O   |\n" +
+                    "|   |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "=========\n",
 
-    " +---+\n" +
-    " |   |\n" +
-    " O   |\n" +
-    "/|   |\n" +
-    "     |\n" +
-    "     |\n" +
-    " =========\n",
+            " +---+\n" +
+                    " |   |\n" +
+                    " O   |\n" +
+                    "/|   |\n" +
+                    "     |\n" +
+                    "     |\n" +
+                    " =========\n",
 
-    " +---+\n" +
-    " |   |\n" +
-    " O   |\n" +
-    "/|\\  |\n" + //if you were wondering, the only way to print '\' is with a trailing escape character, which also happens to be '\'
-    "     |\n" +
-    "     |\n" +
-    " =========\n",
+            " +---+\n" +
+                    " |   |\n" +
+                    " O   |\n" +
+                    "/|\\  |\n" + // if you were wondering, the only way to print '\' is with a trailing escape
+                                  // character, which also happens to be '\'
+                    "     |\n" +
+                    "     |\n" +
+                    " =========\n",
 
-    " +---+\n" +
-    " |   |\n" +
-    " O   |\n" +
-    "/|\\  |\n" +
-    "/    |\n" +
-    "     |\n" +
-    " =========\n",
+            " +---+\n" +
+                    " |   |\n" +
+                    " O   |\n" +
+                    "/|\\  |\n" +
+                    "/    |\n" +
+                    "     |\n" +
+                    " =========\n",
 
-    " +---+\n" +
-    " |   |\n" +
-    " O   |\n" +
-    "/|\\  |\n" + 
-    "/ \\  |\n" +
-    "     |\n" +
-    " =========\n"};
+            " +---+\n" +
+                    " |   |\n" +
+                    " O   |\n" +
+                    "/|\\  |\n" +
+                    "/ \\  |\n" +
+                    "     |\n" +
+                    " =========\n" };
 
     public static void main(String[] args) {
+        String word = word(words);
+        char[] hiddenWord = startGame(word);
+        String strHiddenWord = "";
+        for (int i = 0; i < hiddenWord.length; i++) {
+            strHiddenWord += hiddenWord[i] + " ";
+        }
+        System.out.println(gallows[0]);
+        System.out.println("Word: " + strHiddenWord);
+        System.out.println("Misses: ");
+        System.out.println("Guess: ");
+    }
 
+    public static String word(String[] words) {
+        return words[(int) (Math.random() * words.length) + 1];
+    }
 
-
-
-
-
+    public static char[] startGame(String word) {
+        char[] hiddenWord = new char[word.length()];
+        for (int i = 0; i < hiddenWord.length; i++) {
+            hiddenWord[i] = '_';
+        }
+        return hiddenWord;
     }
 
 }
-
-
-
-
-
