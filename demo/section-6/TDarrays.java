c4b4aa65e62cc40cd89a8c1c340705098138b2eb
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2024-01-02 10:08:17 +0100

    save

--- demo/section-6/TDarrays.java
+++ demo/section-6/TDarrays.java
@@ -4,9 +4,9 @@ public class TDarrays {
     public static void main(String[] args) {
 
         int[][] grades1 = {
-            {72, 74, 78, 76},
-            {65, 64, 61, 67},
-            {95, 98, 99, 100},
+                { 72, 74, 78, 76 },
+                { 65, 64, 61, 67 },
+                { 95, 98, 99, 100 },
         };
 
         int[][] grades = new int[3][4];
@@ -34,9 +34,25 @@ public class TDarrays {
 
         System.out.println("\n");
 
-        System.out.println("\tHarry: " + grades1[0][0] + " " + grades1[0][1] + " " + grades1[0][2] + " " + grades1[0][3]);
-        System.out.println("\tRon: " + grades1[1][0] + " " + grades1[1][1] + " " + grades1[1][2] + " " + grades1[1][3]);
-        System.out.println("\tHermione: " + grades1[2][0] + " " + grades1[2][1] + " " + grades1[2][2] + " " + grades1[2][3]);
+        // System.out.println("\tHarry: " + grades1[0][0] + " " + grades1[0][1] + " " +
+        // grades1[0][2] + " " + grades1[0][3]);
+        // System.out.println("\tRon: " + grades1[1][0] + " " + grades1[1][1] + " " +
+        // grades1[1][2] + " " + grades1[1][3]);
+        // System.out.println("\tHermione: " + grades1[2][0] + " " + grades1[2][1] + " "
+        // + grades1[2][2] + " " + grades1[2][3]);
+
+        for (int i = 0; i < grades1.length; i++) {
+
+            switch (i) {
+                case 0: System.out.println("\tHarry: "); break;
+                case 1: System.out.println("\tRon: " ); break;
+                case 2: System.out.println("\tHermione: "); break;
+            }
+            for (int j = 0; j < grades1[i].length; j++) {
+                System.out.print(grades1[i][j] + " ");
+            }
+            System.out.println("\n");
+        }
 
     }
 }
