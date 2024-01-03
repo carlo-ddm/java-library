125abccb66f4d1a069a53f3651ea554e11c57e22
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2024-01-03 11:33:20 +0100

    save

--- demo/section-7 (OOP)/Car.java
+++ demo/section-7 (OOP)/Car.java
@@ -11,32 +11,37 @@ public class Car {
         this.color = color;
     }
 
+
     public String getMake() {
         return this.make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
     }
 
     public double getPrice() {
         return this.price;
     }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     public int getYear() {
         return this.year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
     public String getColor() {
         return this.color;
     }
 
-    public void setMake(String make) {
-        this.make = make;
-    }
-    public void setPrice(double price) {
-        this.price = price;
-    }
-    public void setYear(int year) {
-        this.year = year;
-    }
     public void setColor(String color) {
         this.color = color;
     }
+
 }
