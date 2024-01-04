80979148e974f34520d2de1eb90dc64aa1bead19
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2024-01-04 17:18:20 +0100

    save

--- workbooks/Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.8/Airline.java
+++ workbooks/Module 2 - Object Oriented Programming/7. The World of Objects/workbooks/Workbook 7.8/Airline.java
@@ -1,6 +1,27 @@
 public class Airline {
-    
-    Person[] people; //array that stores Person objects...
 
+    Person[] people; // array that stores Person objects...
+
+    public Airline() {
+        this.people = new Person[11];
+    }
+
+    public Person getPerson(int index) {
+        return new Person(this.people[index]);
+    }
+
+    /**
+     * Setter name: setPerson
+     *
+     * @param person
+     *
+     *               Inside the setter:
+     *
+     *               1. Index the array, and set that element equal to the Person
+     *               object.
+     */
+    public void setPerson(Person person) {
+        this.people[person.getSeatNumber() - 1] = new Person(person);
+    }
 
 }
