f75dcf92ca9f2ed68966837e21f04793f719b60d
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2024-01-16 14:45:53 +0100

    save

--- workbooks/Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/Main.java
+++ workbooks/Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/Main.java
@@ -1,9 +1,25 @@
+import product.Shirt;
+import static product.Shirt.Size.*;
+
+import product.Pants;
+
 public class Main {
 
     public static void main(String[] args) {
+        Shirt shirt = new Shirt();
+        shirt.setSize(LARGE);
+        shirt.setBrand("NIKE");
+        shirt.setPrice(49.99);
+        shirt.setColor("Blue");
+        shirt.fold();
 
-
-
+        Pants pants = new Pants();
+        pants.setWaist(48);
+        pants.setLength(71);
+        pants.setBrand("LEVI'S");
+        pants.setPrice(120.99);
+        pants.setColor("Black");
+        pants.fold();
     }
 
 }
