45678fc1ab1fbf95deb9d7ef9a6758fdc2d17eda
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2024-01-17 10:06:59 +0100

    save

--- workbooks/Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/Main.java
+++ workbooks/Module 2 - Object Oriented Programming/15. Inheritance/Lesson/starter/Main.java
@@ -2,10 +2,12 @@ import product.Shirt;
 import static product.Shirt.Size.*;
 
 import product.Pants;
+import product.Product;
 
 public class Main {
 
     public static void main(String[] args) {
+        // Product shirt = new Shirt();
         Shirt shirt = new Shirt();
         shirt.setSize(LARGE);
         shirt.setBrand("NIKE");
@@ -13,6 +15,7 @@ public class Main {
         shirt.setColor("Blue");
         shirt.fold();
 
+        // Product pants = new Pants();
         Pants pants = new Pants();
         pants.setWaist(48);
         pants.setLength(71);
@@ -20,6 +23,27 @@ public class Main {
         pants.setPrice(120.99);
         pants.setColor("Black");
         pants.fold();
+
+        // pantStore(pants);
+        // shirtStore(shirt);
+
+        productStore(pants);
+        productStore(shirt);
     }
 
+
+    // public static void pantStore(Pants pants) {
+    //     System.out.println("Thank you for purchasing " + pants.getBrand() + " Pants. " + "Your total comes to "
+    //             + pants.getPrice());
+    // }
+
+    // public static void shirtStore(Shirt shirt) {
+    //     System.out.println("Thank you for purchasing " + shirt.getBrand() + " Shirt. " + "Your total comes to "
+    //             + shirt.getPrice());
+    // }
+
+    public static void productStore(Product product) {
+        System.out.println("Thank you for purchasing " + product.getBrand() + " " + product.getClass().getSimpleName()+ ". Your total comes to "
+                + product.getPrice());
+    }
 }
