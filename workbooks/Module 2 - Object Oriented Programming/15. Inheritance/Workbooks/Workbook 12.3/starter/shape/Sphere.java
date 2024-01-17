cc8f7cc37da566e93c47970dbfc956de9c13ed13
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2024-01-17 15:06:41 +0100

    save

--- workbooks/Module 2 - Object Oriented Programming/15. Inheritance/Workbooks/Workbook 12.3/starter/shape/Sphere.java
+++ workbooks/Module 2 - Object Oriented Programming/15. Inheritance/Workbooks/Workbook 12.3/starter/shape/Sphere.java
@@ -1,8 +1,21 @@
 package shape;
+
 public class Sphere extends Shape {
 
     public Sphere(double radius) {
         super(radius);
+    }
+
+    @Override
+    public double getArea() {
+        double radius = getRadius();
+        return 4 * Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getVolume() {
+        double radius = getRadius();
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
     }
 
 }
