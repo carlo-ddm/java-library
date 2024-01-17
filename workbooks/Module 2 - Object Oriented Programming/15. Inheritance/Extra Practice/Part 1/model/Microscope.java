3d8bb9ce9d855e9661161f38aafee283bc22fcec
Carlo Duminuco Del Monte <c.duminucodelmonte@teamsystem.com>
2024-01-17 15:35:38 +0100

    save

---
+++ workbooks/Module 2 - Object Oriented Programming/15. Inheritance/Extra Practice/Part 1/model/Microscope.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Microscope extends LabEquipment {
+    private int magnification;
+
+    public static final int MIN_MAGNIFICATION = 5;
+
+    public Microscope(String manufacturer, String model, int year, int magnification) {
+        super(manufacturer, model, year);
+        setMagnification(magnification);
+    }
+
+
+    public int getMagnification() {
+        return magnification;
+    }
+
+    public void setMagnification(int magnification) {
+        if (magnification < MIN_MAGNIFICATION) {
+            throw new IllegalArgumentException("Magnification must be greater than or equal to the minimum magnification.");
+        }
+        this.magnification = magnification;
+    }
+
+    @Override
+    public String performMaintenance() {
+        // TODO Auto-generated method stub
+        return "Microscope maintenance: Clean the lenses and check the light source.";
+    }
+
+}
